1f9840fca3e337246b846b7382c03c5bdea74c7f
sijoyjoseph <joseph.sijoy3@gmail.com>
2017-08-27 17:03:15 +0530

    basic schema creation

---
+++ basic_1.py
@@ -0,0 +1,13 @@
+import graphene
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(name=graphene.Argument(graphene.String, default_value="stranger"))
+
+    def resolve_hello(self, args, context, info):
+        return 'Hello '+args['name']
+
+
+schema = graphene.Schema(query=Query)
+
+result = schema.execute('{ hello }')
+print (result.data['hello'])
